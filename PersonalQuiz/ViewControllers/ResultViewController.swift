e6a4ed97913860a4df92c24267532eebe2e630c8
VorchunV <varenkoluchka@gmail.com>
2021-11-03 18:28:25 +0300

    most common answer

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var answerChosen: [Answer]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        countingTheResult()
+        
+    }
+    
+    func countingTheResult() {
+        var countingSelectedAnimals: [AnimalType: Int] = [:]
+        let animals = answerChosen.map{ $0.type }
+        
+        for animal in animals {
+            countingSelectedAnimals[animal] = (countingSelectedAnimals[animal] ?? 0) + 1
+            
+        }
+        let sortingResponse = countingSelectedAnimals.sorted {$0.value > $1.value}
+        let mostCommonAnswer = sortingResponse.first!.key
         
         
     }
-
+    
+    
+    
     // 1. Передать сюда массив с ответами +
     // 2. Определить наиболее часто встречающийся тип животного
     // 3. Отобразить результаты в соотвствии с этим животным
